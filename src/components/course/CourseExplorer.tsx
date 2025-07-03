2d19a6cc279333967d5baf14bbf24f4b37e12218
GiSeok Kang <ime.rokz@gmail.com>
2025-07-03 11:02:57 +0900

    update

--- src/components/course/CourseExplorer.tsx
+++ src/components/course/CourseExplorer.tsx
@@ -67,6 +67,7 @@ export const CourseExplorer = () => {
           </div>
         </div>
 
+        <pre style={{fontSize: 10, color: 'red'}}>{JSON.stringify(displayCourses, null, 2)}</pre>
         <div className="space-y-4 max-h-[600px] overflow-y-auto">
           {displayCourses.length === 0 ? (
             <div className="text-center py-8">
